fc181cb72549e56d27af6aae277288c6fad32c53
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2025-05-01 15:30:55 +0300

    Initial Commit: Project files added.

---
+++ app/src/main/java/com/example/loginscreen/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.loginscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.loginscreen.MainActivity // BUNU EKLE
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
